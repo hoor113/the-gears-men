6ff5672d14227f6327d4418cabc8c4c91de30be3
Tran Trong Nguyen <kenzoropp@gmail.com>
2025-05-04 08:58:35 +0700

    css for discount prices

--- frontend/src/pages/app/customer/_components/product-item.tsx
+++ frontend/src/pages/app/customer/_components/product-item.tsx
@@ -20,13 +20,21 @@ const ProductItem: React.FC<ProductProps> = ({ product }) => {
                 <div className="overlay"></div>
                 <div className='button-overlay'>
                     <div className='detail' onClick={showProductModal}>Chi tiết</div>
-                    <div className='more' onClick={() => {console.log("Xem thêm")}}>Xem thêm</div>
+                    <div className='more' onClick={() => { console.log("Xem thêm") }}>Xem thêm</div>
                 </div>
             </div>
-            <div className="content">
-                <div className='name'>{product.name}</div>
-                <div className='price'>{product.price} VNĐ</div>
-            </div>
+            {product.priceAfterDiscount == undefined ?
+                (<div className="content">
+                    <div className='name'>{product.name}</div>
+                    <div className='price'>{product.price} VNĐ</div>
+                </div>)
+                :
+                (<div className="content-discount">
+                    <div className='name'>{product.name}</div>
+                    <div className='price'>{product.price} VNĐ</div>
+                    <div className="discount-price">{product.priceAfterDiscount} VNĐ</div>
+                </div>)
+            }
         </div>
     );
 };

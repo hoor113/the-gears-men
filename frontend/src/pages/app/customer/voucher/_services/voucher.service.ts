803ee1ffd47e7ffc7cf7fe24ceab0a2d87abb81e
Tran Trong Nguyen <kenzoropp@gmail.com>
2025-05-16 08:21:58 +0700

    update voucher page

--- frontend/src/pages/app/customer/voucher/_services/voucher.service.ts
+++ frontend/src/pages/app/customer/voucher/_services/voucher.service.ts
@@ -3,23 +3,39 @@ import { httpService } from '@/base/http-service';
 // import { IPaginatedItems } from '@/base/base.model';
 // import { httpService } from '@/base/http-service';
 
+interface DiscountCode {
+  id: String
+}
+
 class VoucherService extends BaseCrudService {
-    constructor() {
-        super('/discount-codes');
-        
-    }
+  constructor() {
+    super('/discount-codes');
 
-    public async getDiscountCodesOfCustomer<T>(
-        path = '/customer',
-      ): Promise<{ data: T }> {
-        console.log("hard code", `${this.basePath}${path}`);
-        const res = await httpService.request<TBaseResponse<{ data: T }>>({
-          method: 'GET',
-          url: `${this.basePath}${path}`,
-        });
+  }
 
-        return res.result;
-      }
+  public async getDiscountCodesOfCustomer<T>(
+    path = '/customer',
+  ): Promise<{ data: T }> {
+    console.log("hard code", `${this.basePath}${path}`);
+    const res = await httpService.request<TBaseResponse<{ data: T }>>({
+      method: 'GET',
+      url: `${this.basePath}${path}`,
+    });
+
+    return res.result;
+  }
+
+  public async claimDiscountCode<T>(data: any, path = '/claim') {
+    const res = await httpService.request<TBaseResponse<T>>({
+      method: 'POST',
+      url: `${this.basePath}${path}`,
+      data,
+    });
+
+    return res.result;
+
+  }
+
 }
 
 const voucherService = new VoucherService();

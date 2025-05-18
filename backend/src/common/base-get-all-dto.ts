e363baecd69fbafc59004615cb816ba1af891f38
hoor113 <nguyenlekhoi2004113@gmail.com>
2025-05-19 00:04:07 +0700

    Try merge

--- backend/src/common/base-get-all-dto.ts
+++ backend/src/common/base-get-all-dto.ts
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 import 'reflect-metadata';
-=======
->>>>>>> e65dd44d474f28c07dc6c6d97dd41a4f565a984c
 import { Type } from 'class-transformer';
 import { IsNumber, IsOptional, IsString, Min } from 'class-validator';
 import 'reflect-metadata';
